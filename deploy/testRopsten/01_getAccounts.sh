f79496e9b3effda8d4498a5e7cda4ebfab854716
BokkyPooBah <bokky.poobah@bokconsulting.com.au>
2019-03-09 04:07:09 +1100

    Ropsten testing WIP

--- deploy/testRopsten/01_getAccounts.sh
+++ deploy/testRopsten/01_getAccounts.sh
@@ -60,7 +60,7 @@ if ("$MODE" == "addorder") {
   var uiFeeAccount = "$UIFEEACCOUNT";
   var tradeData = dexz.trade.getData(orderFlag, weenus.address, xeenus.address, buyPrice, expiry, buyAmount, uiFeeAccount, {from: "$TEST1ACCOUNT", gas: 3000000, gasPrice: web3.toWei(5, "gwei")});
   console.log("RESULT: tradeData[Buy 1500 WEENUS @ WEENUS/XEENUS]='" + tradeData + "'");
-  var approveAndCall1 = zeenus.approveAndCall(dexzAddress, buyAmount, tradeData, {from: "$TEST1ACCOUNT", gas: 2000000, gas: 1000000, gasPrice: web3.toWei(5, "gwei")});
+  var approveAndCall1 = xeenus.approveAndCall(dexzAddress, buyAmount, tradeData, {from: "$TEST1ACCOUNT", gas: 2000000, gas: 1000000, gasPrice: web3.toWei(5, "gwei")});
   while (eth.getTransactionReceipt(approveAndCall1) == null) {
   }
 
