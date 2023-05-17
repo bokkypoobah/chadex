2ef61246fa93cdbee677b0b4aea21ead6d916ee7
BokkyPooBah <bokky.poobah@bokconsulting.com.au>
2023-05-17 10:33:43 +1000

    BPBRBT Solidity ^0.8.0

--- contracts/BokkyPooBahsRedBlackTreeLibrary.sol
+++ contracts/BokkyPooBahsRedBlackTreeLibrary.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.6.0;
+pragma solidity ^0.8.0;
 
 // ----------------------------------------------------------------------------
 // BokkyPooBah's Red-Black Tree Library v1.0-pre-release-a
