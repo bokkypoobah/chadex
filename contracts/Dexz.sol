e3b7df038253f0814f8686002f192be09b09b0d6
BokkyPooBah <bokky.poobah@bokconsulting.com.au>
2023-05-23 00:59:35 +1000

    wip

--- contracts/Dexz.sol
+++ contracts/Dexz.sol
@@ -20,6 +20,10 @@ interface IERC20 {
     event Transfer(address indexed from, address indexed to, uint tokens);
     event Approval(address indexed tokenOwner, address indexed spender, uint tokens);
 
+    function name() external view returns (string memory);
+    function symbol() external view returns (string memory);
+    function decimals() external view returns (uint8);
+    
     function totalSupply() external view returns (uint);
     function balanceOf(address tokenOwner) external view returns (uint balance);
     function allowance(address tokenOwner, address spender) external view returns (uint remaining);
